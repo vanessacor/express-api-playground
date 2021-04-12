a996fd58f56392ed67d8e6fd748146743757912c
vanessa <vanessapcbarros@gmail.com>
2021-04-12 15:39:26 +0200

    Feat: create .env file and server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const createApp = require("./app.js");
 
-const port = 3000
+const port = process.env.PORT;
 
 const app = createApp(console);
-app.listen(port, () => {
+app.listen(port || 3001, () => {
   console.log(`Example app listening on ${port}`);
 });
+
